c061ea6d6eb5c7768bc489eeb182945b0a74c51c
James Gough <jpgough@gmail.com>
2022-12-08 19:57:53 +0000

    #4 Add GetAttendee

--- src/main/java/com/masteringapi/attendees/grpc/AttendeesServiceImpl.java
+++ src/main/java/com/masteringapi/attendees/grpc/AttendeesServiceImpl.java
@@ -1,13 +1,18 @@
 package com.masteringapi.attendees.grpc;
 
 import com.masteringapi.attendees.grpc.server.*;
+import com.masteringapi.attendees.model.AttendeeNotFoundException;
 import com.masteringapi.attendees.service.AttendeeStore;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @GrpcService
 public class AttendeesServiceImpl extends AttendeesServiceGrpc.AttendeesServiceImplBase {
+
+    private Logger logger = LoggerFactory.getLogger(AttendeesServiceImpl.class);
 
     private AttendeeStore store;
 
@@ -47,5 +52,24 @@ public class AttendeesServiceImpl extends AttendeesServiceGrpc.AttendeesServiceI
         responseObserver.onCompleted();
     }
 
+    @Override
+    public void getAttendee(GetAttendeeRequest request, StreamObserver<GetAttendeeResponse> responseObserver) {
+        GetAttendeeResponse.Builder responseBuilder = GetAttendeeResponse.newBuilder();
 
+        try {
+            com.masteringapi.attendees.model.Attendee attendee = this.store.getAttendee(request.getId());
+            Attendee grpcAttendee = Attendee.newBuilder()
+                    .setId(attendee.getId())
+                    .setGivenName(attendee.getGivenName())
+                    .setSurname(attendee.getSurname())
+                    .setEmail(attendee.getEmail())
+                    .build();
+            responseBuilder.setAttendee(grpcAttendee);
+            responseObserver.onNext(responseBuilder.build());
+        } catch (AttendeeNotFoundException e) {
+            responseObserver.onError(e);
+            logger.error("Could not find attendee", e);
+        }
+        responseObserver.onCompleted();
+    }
 }
